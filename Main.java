c3986b96be3d42d0195f75263818d537ebc4ed26
Ryder <ryderstanley2007@gmail.com>
2020-04-10 14:05:18 -0600

    added commands for movement

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
+import lwjgl;
+import scanner;
+public class Main {
+  public static void Main(String[] args) {
+    int height = 407;
+    int width = 427;
+    int x;
+    int y;
+    int up = x + 1;
+    int down = x - 1;
+    int right = x + 1, y - 1;
+    int left = x - 1, y + 1;
+    int upleft = x + 1, y + 1 ;
+    int upright = x - 1, y - 1;
+    int downleft = x - 2, y + 1;
+    int downright = x + 2, y - 1;
 
+  }
+}
