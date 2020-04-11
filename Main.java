a96cdd2122104a58bd204107d4783ba4c991e3ef
Ryder <ryderstanley2007@gmail.com>
2020-04-10 22:56:23 -0600

    finished the game loop, started the game over loop

--- Main.java
+++ Main.java
@@ -90,13 +90,88 @@ public class Main {
         head = iih.getImage();
       }
 
+      // game loop function value
       private void initGame() {
+        // adds three dots after eating an apple
         dots = 3;
-
+        // when an apple is eaten it adds the value
         for (int z = 0; z < dots; z++) {
+          // x's value multiplies by 10 when apple is eaten
           x[z] = 50 - z * 10;
+          // y's value is 50 when apple is eaten
           y[z] = 50;
         }
+
+        // forces timer for game loop
+        locateApple();
+
+        // the timer delays the snake
+        timer = new Timer(DELAY, this);
+        // the timer starts
+        timer.start();
+      }
+
+      // overrides the user's front for backend fixes
+      @Override
+
+      // adds colored graphics
+      public void paintComponent(Graphics g) {
+        // the paint has the graphics of g
+        super.paintComponent(g);
+
+        // loads drawing
+        doDrawing(g);
+      }
+
+      // adds g graphics to the drawing function
+      private void doDrawing(Graphics g) {
+
+        // if the user is running the program, it enters the game loop
+        if (inGame) {
+
+          // it draws the image of the apple
+          g.drawImage(apple, apple_x, apple_y, this);
+
+          // the z axis allows the user to move
+          for (int z = 0; z < dots; z++) {
+            // if the user does not move, it enters a AFK loop
+            if (z == 0) {
+              // it draws the image of the head of the snake
+              g.drawImage(head, x[z], y[z], this);
+              // if the user dies, it enters this long loop, to kill off the program
+            else {
+              // it draws the image of a ball
+              g.drawImage(ball, x[z], y[z], this);
+              }
+            }
+
+            // it syncs the toolkit to the backend
+            Toolkit.getDefaultToolkit().sync();
+
+          }
+          // it cuts the program using the function, defined below
+          else {
+            gameOver(g);
+          }
+        }
+      }
+
+      // the value of the game cut function
+      private void gameOver() {
+
+        // the string message of the game over
+        String msg = "Game Over!";
+        // font options
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        // font metrics
+        FontMetrics metr = getFontMetrics(small);
+
+        // the font color is white
+        g.setColor(Color.white);
+        // the font size is small
+        g.setFont(small);
+        // draws the msg string with our font options
+        g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2);
       }
 
 // compile allow for java
